080574a757d6b9a803c64bc263c0482457e07f6a
mazgamer1 <76127332+mazgamer1@users.noreply.github.com>
2025-02-05 19:34:00 -0800

    Delete ores.js

--- ores.js
+++
@@ -1,3 +0,0 @@
-const ores = {
-"4ldqpgu|4lpndhc|lfls|5yc22|iosoon|4ldqpds|1nje1c|3cx5qr|ep8179h|2s|55e57cy|4lec507|i6wtfu|ntzcq|x8pbgx|4qccrv8|96rhmhc|2cvl|1tjuhg|cibhx|oersx|4lec4zo|3|4n1a1vk|icvgfs|6jrnl|3n1i8x|1983f|lfls|6jrt4|gjdwdc|5yc1s|2klc|s2|gjftu6|639lt|4rzyikx|3b94gw|x4k3mu|6n7e|gjfm6d|4ym294|21368|60rbg":[["Viridian", 0, -1262, -1], ["Viridian", 0, -1262, 1]]
-}
